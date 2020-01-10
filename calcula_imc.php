1a6f8e9bbb288a6cd75924999561457538fff6a7
Leonardo Vilani <leonardovilani@sedep.com.br>
2020-01-10 10:41:43 -0400

    commit inicial

---
+++ calcula_imc.php
@@ -0,0 +1,71 @@
+﻿<?php
+
+echo "Aqui esta o seu índice de massa corpória!</p>";
+
+$imc = 0;
+$altura = $_POST['altura'];
+$peso = $_POST['peso'];
+$idade = $_POST['idade'];
+$altura2= $altura*$altura;
+
+$imc=$peso/$altura2;
+
+echo $imc."</p>";
+
+
+if($idade < 21)
+{
+	if($imc < 10)
+	{
+		echo "ABAIXO DO PESO";
+	}
+	elseif($imc < 85)
+	{
+		echo "PESO NORMAL";
+	}
+	elseif($imc < 95)
+	{
+		echo "SOBREPESO";
+	}
+	elseif($imc > 95)
+	{
+		echo "OBESIDADE";
+	}
+}
+
+else 
+{
+	if ($imc < 17)
+	{
+		echo "MUITO ABAIXO DO PESO";
+	}
+	elseif($imc < 18.50)
+	{
+		echo "ABAIXO DO PESO";
+	}
+	elseif($imc < 25)
+	{
+		echo "PESO NORMAL";
+	}
+
+	elseif($imc < 30)
+	{
+		echo "ACIMA DO PESO";
+	}
+	
+	elseif($imc < 35)
+	{
+		echo "OBESIDADE I";
+	}
+	
+	elseif($imc < 40)
+	{
+		echo "OBESIDADE II (SEVERA)";
+	}	
+	elseif ($imc>40)
+	{
+		echo "OBESIDADE III (MORBIDA)";
+	}
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+﻿<html>
+<form action="calcula_imc.php" method="POST"> 
+<table align=center border=1>
+<tr>
+	<td>Idade</td><td><input type=text name=idade ></td>
+</tr>
+<tr>
+	<td>Altura</td><td><input type=text name=altura ></td>
+</tr>
+<tr>
+	<td>Peso</td><td><input type=text name= peso></td>
+</tr>
+<tr>
+	<td></td><td><input type=submit name= enviar value=calcular></td>
+</tr>
+</table>
+</form>
+</html>
